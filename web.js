fcf94f82094ec0e49399fe825f9b4f4241dec193
Ravi Jain <jain.rav@gmail.com>
2013-07-10 19:24:28 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,8 +1,8 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
-var out = ReadFileSync('index.html');
+var out = fs.readFileSync('index.html');
 var display = out.toString();
 
 app.get('/', function(request, response) {
