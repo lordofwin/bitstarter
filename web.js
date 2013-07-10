6be724596f395e653ee02de123642bb349156ace
Ravi Jain <jain.rav@gmail.com>
2013-07-10 19:21:22 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var out = ReadFileSync('index.html');
+var display = out.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(display);
 });
 
 var port = process.env.PORT || 5000;
